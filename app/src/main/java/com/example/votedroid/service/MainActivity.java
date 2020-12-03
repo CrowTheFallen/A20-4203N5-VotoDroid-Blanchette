c4e3f825b5755b2ed5c8bd353f069ca92f8ea991
Alex Blanchette <bigue1994@hotmail.com>
2020-12-03 14:41:21 -0500

    PROGRES Ajout d'un menu dans MainActivity
    Ajout d'une liste contenant l'option de supprimer une question et une pour un vote (Ne fonctionnant pas).

---
+++ app/src/main/java/com/example/votedroid/service/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.votedroid.service;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.votedroid.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.Supprime_votes) {
+
+            return true;
+        }
+        if (id == R.id.Supprime_Question) {
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
